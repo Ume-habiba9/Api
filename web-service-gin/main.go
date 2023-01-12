6ef45904ef1d09a7370cf1e9c26dd5f4187634d1
Ume-habiba9 <habibaume571@gmail.com>
2023-01-12 14:41:40 +0500

    --

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
+	uuid "github.com/satori/go.uuid"
 )
 
-type car struct {
+type Car struct {
 	ID       string  `json:"id"`
 	CarName  string  `json:"carname"`
 	CarType  string  `json:"cartype"`
@@ -15,55 +15,62 @@ type car struct {
 	Price    float64 `json:"price"`
 }
 
-var cars = []car{
-	{ID: "1", CarName: "Nissan GT - R", CarType: "Sport", Capacity: "2 People", Price: 56.99},
-	{ID: "2", CarName: "CR  - V", CarType: "SUV", Capacity: "4 People", Price: 17.99},
-	{ID: "3", CarName: "MG ZX Excite", CarType: "Hatchback", Capacity: "4 people", Price: 39.99},
+var Cars map[uuid.UUID]Car
+
+func init() {
+	Cars = make(map[uuid.UUID]Car)
+	id1 := uuid.NewV4()
+	Cars[id1] = Car{id1.String(), "Tesla", "SUV", "6 People", 6.78}
+	id2 := uuid.NewV4()
+	Cars[id2] = Car{id2.String(), "BMW", "MPV", "4 people", 63.89}
+	id3 := uuid.NewV4()
+	Cars[id3] = Car{id3.String(), "Mercedes", "Hatchback", "6 people", 54.9}
 }
 
-// get all cars
-func getCars(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, cars)
+// get all Cars
+func getallCars(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Cars)
 }
 
-// add new cars
-func postCars(c *gin.Context) {
-	var newCar car
-	newCar.ID = CreateUUID()
+// add new Cars
+func postCar(c *gin.Context) {
+	var newCar Car
+	id := uuid.NewV4()
+	newCar.ID = id.String()
 	if err := c.BindJSON(&newCar); err != nil {
+		Cars[id] = newCar
 		return
 	}
-	cars = append(cars, newCar)
 	c.IndentedJSON(http.StatusCreated, newCar)
 }
-func getaCar(c *gin.Context) {
+
+func getCar(c *gin.Context) {
 	id := c.Param("id")
-	for _, getcar := range cars {
-		if getcar.ID == id {
-			c.IndentedJSON(http.StatusOK, getcar)
+	for i, car := range Cars {
+		if car.ID == id {
+			c.IndentedJSON(http.StatusOK, i)
 			return
 		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "car not found"})
 }
-func deleteCars(c *gin.Context) {
-	id := c.Param("id")
-	for _, deletecar := range cars {
-		if deletecar.ID == id {
-			c.MultipartForm()
+
+func deleteCar(c *gin.Context) {
+	i := c.Param("id")
+	for car, deletecar := range Cars {
+		if deletecar.ID == i {
+			delete(Cars, car)
+			c.JSON(http.StatusOK, gin.H{"message": "Car deleted!!"})
+			return
 		}
 		c.JSON(http.StatusOK, gin.H{"message": "Car not found"})
 	}
 }
-func CreateUUID() string {
-	id := uuid.New()
-	return id.String()
-}
 func main() {
 	router := gin.Default()
-	router.GET("/cars", getCars)
-	router.POST("/cars", postCars)
-	router.GET("/cars/id", getaCar)
-	router.DELETE("/cars/id", deleteCars)
+	router.GET("/Cars", getallCars)
+	router.POST("/Cars", postCar)
+	router.GET("/Cars/:id", getCar)
+	router.DELETE("/Cars/:id", deleteCar)
 	router.Run("localhost:8080")
 }
