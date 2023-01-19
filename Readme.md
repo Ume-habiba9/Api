bc825dba81087114ed5e84f36fda2d16f54563d1
Ume-habiba9 <habibaume571@gmail.com>
2023-01-19 11:43:02 +0500

    --

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Creating RESTful API with [Go](https://go.dev/) and [Gin](https://gin-gonic.com/)
+
+## Getting Started
+
+Firt run the development server:
+```go run ./main.go```
+ To send Api Requests you need to use [Postman](https://www.postman.com/)
--- main.go
+++ main.go
@@ -10,68 +10,6 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-func getallCars(c *gin.Context) {
-	database := db.DBConnect()
-	defer database.Close()
-	cars, err := db.GetCarsfromDB()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	c.IndentedJSON(http.StatusOK, cars)
-}
-
-func postCar(c *gin.Context) {
-	var newCar db.Car
-	database := db.DBConnect()
-	defer database.Close()
-	id := uuid.NewV4()
-	newCar.ID = id.String()
-	if err := c.BindJSON(&newCar); err != nil {
-		return
-	}
-	err := db.PostcarinDB(db.Car(newCar))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	c.IndentedJSON(http.StatusCreated, newCar)
-}
-
-func getCar(c *gin.Context) {
-	id := c.Param("id")
-	car, err := db.GetcarfromDB(id)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	c.IndentedJSON(http.StatusOK, car)
-}
-
-func deleteCar(c *gin.Context) {
-	i := c.Param("id")
-	err := db.DeletecarfromDB(i)
-	if err != nil {
-		c.JSON(http.StatusOK, err)
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"message": "Car deleted!!"})
-}
-func updateCar(c *gin.Context) {
-	id := c.Param("id")
-	var cardata db.Car
-	if err := c.ShouldBindJSON(&cardata); err != nil {
-		fmt.Println("errr ", err)
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	err := db.UpdatecarinDB(id, db.Car(cardata))
-	if err != nil {
-		fmt.Println(err)
-		c.JSON(http.StatusInternalServerError, err)
-	}
-	c.IndentedJSON(http.StatusOK, "Updated Successfully")
-}
 func main() {
 	router := gin.Default()
 	router.GET("/Cars", getallCars)
