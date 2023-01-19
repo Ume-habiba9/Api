cddd56b3d9799a27df16580a2af3e3eff0c29c29
Ume-habiba9 <habibaume571@gmail.com>
2023-01-19 10:46:06 +0500

    updated code

--- db/db.go
+++ db/db.go
@@ -1,6 +1,78 @@
 package db
-import "fmt"
 
-func data()  {
-	fmt.Println("My data")
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Car struct {
+	ID       string  `json:"id" db:"car_id"`
+	CarName  string  `json:"carname" db:"car_name"`
+	CarType  string  `json:"cartype" db:"car_type"`
+	Capacity string  `json:"capacity" db:"capacity"`
+	Price    float64 `json:"price" db:"price"`
+	GasType  string  `json:"gastype" db:"gas_type"`
+}
+
+func DBConnect() *sqlx.DB {
+	db, err := sqlx.Connect("postgres", "user=postgres password=cactus1470 host=localhost port=5432 dbname=carrental sslmode=disable")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return db
+}
+func GetCarsfromDB() ([]*Car, error) {
+	db := DBConnect()
+	cars := make([]*Car, 0)
+	err := db.Select(&cars, `SELECT car_id, car_name, car_type, capacity,price,gas_type FROM carrental.cars`)
+	if err != nil {
+		return nil, err
+	}
+	return cars, nil
+}
+func PostcarinDB(car Car) error {
+	database := DBConnect()
+	defer database.Close()
+	fmt.Println(car)
+	query := `INSERT INTO carrental.cars (car_id, car_name, car_type, capacity,price,gas_type) VALUES (:car_id, :car_name, :car_type, :capacity,:price,:gas_type)`
+	_, err := database.NamedExec(query, car)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func GetcarfromDB(id string) ([]*Car, error) {
+	database := DBConnect()
+	defer database.Close()
+	car := make([]*Car, 0)
+	query := `SELECT car_id, car_name, car_type, capacity,price,gas_type FROM carrental.cars WHERE car_id= $1`
+	err := database.Select(&car, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return car, nil
+}
+func DeletecarfromDB(id string) error {
+	database := DBConnect()
+	defer database.Close()
+	query := `DELETE FROM carrental.cars WHERE car_id=$1`
+	_, err := database.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Car Deleted")
+	return nil
+}
+func UpdatecarinDB(id string, cardata Car) error {
+	database := DBConnect()
+	defer database.Close()
+	query := `UPDATE carrental.cars SET car_id=:car_id,car_name=:car_name,car_type=:car_type,capacity=:capacity,price=:price,gas_type=:gas_type WHERE car_id=$1`
+	_, err := database.NamedExec(query, cardata)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -4,134 +4,74 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Ume-habiba9/Api/db"
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
 )
 
-type Car struct {
-	ID       string  `json:"id" db:"car_id"`
-	CarName  string  `json:"carname" db:"car_name"`
-	CarType  string  `json:"cartype" db:"car_type"`
-	Capacity string  `json:"capacity" db:"capacity"`
-	Price    float64 `json:"price" db:"price"`
-	GasType  string  `json:"gastype" db:"gas_type"`
-}
-
 func getallCars(c *gin.Context) {
-	db := DBConnect()
-	defer db.Close()
-	cars, err := getCarsfromDB()
+	database := db.DBConnect()
+	defer database.Close()
+	cars, err := db.GetCarsfromDB()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 	c.IndentedJSON(http.StatusOK, cars)
 }
-func getCarsfromDB() ([]*Car, error) {
-	db := DBConnect()
-	cars := make([]*Car, 0)
-	err := db.Select(&cars, `SELECT car_id, car_name, car_type, capacity,price,gas_type FROM carrental.cars`)
-	if err != nil {
-		return nil, err
-	}
-	return cars, nil
-}
 
 func postCar(c *gin.Context) {
-	var newCar Car
-	db := DBConnect()
-	defer db.Close()
+	var newCar db.Car
+	database := db.DBConnect()
+	defer database.Close()
 	id := uuid.NewV4()
 	newCar.ID = id.String()
 	if err := c.BindJSON(&newCar); err != nil {
 		return
 	}
-	err := postcarinDB(newCar)
+	err := db.PostcarinDB(db.Car(newCar))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 	c.IndentedJSON(http.StatusCreated, newCar)
 }
-func postcarinDB(car Car) error {
-	db := DBConnect()
-	defer db.Close()
-	fmt.Println(car)
-	query := `INSERT INTO carrental.cars (car_id, car_name, car_type, capacity,price,gas_type) VALUES (:car_id, :car_name, :car_type, :capacity,:price,:gas_type)`
-	_, err := db.NamedExec(query, car)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+
 func getCar(c *gin.Context) {
 	id := c.Param("id")
-	car, err := getcarfromDB(id)
+	car, err := db.GetcarfromDB(id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
 	c.IndentedJSON(http.StatusOK, car)
 }
-func getcarfromDB(id string) ([]*Car, error) {
-	db := DBConnect()
-	defer db.Close()
-	car := make([]*Car, 0)
-	query := `SELECT car_id, car_name, car_type, capacity,price,gas_type FROM carrental.cars WHERE car_id= $1`
-	err := db.Select(&car, query, id)
-	if err != nil {
-		return nil, err
-	}
-	return car, nil
-}
+
 func deleteCar(c *gin.Context) {
 	i := c.Param("id")
-	err := deletecarfromDB(i)
+	err := db.DeletecarfromDB(i)
 	if err != nil {
 		c.JSON(http.StatusOK, err)
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Car deleted!!"})
 }
-func deletecarfromDB(id string) error {
-	db := DBConnect()
-	defer db.Close()
-	query := `DELETE FROM carrental.cars WHERE car_id=$1`
-	_, err := db.Exec(query, id)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Car Deleted")
-	return nil
-}
 func updateCar(c *gin.Context) {
 	id := c.Param("id")
-	var cardata Car
+	var cardata db.Car
 	if err := c.ShouldBindJSON(&cardata); err != nil {
 		fmt.Println("errr ", err)
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
-	err := updatecarinDB(id, cardata)
+	err := db.UpdatecarinDB(id, db.Car(cardata))
 	if err != nil {
 		fmt.Println(err)
 		c.JSON(http.StatusInternalServerError, err)
 	}
 	c.IndentedJSON(http.StatusOK, "Updated Successfully")
 }
-func updatecarinDB(id string, cardata Car) error {
-	db := DBConnect()
-	defer db.Close()
-	query := `UPDATE carrental.cars SET car_id=:car_id,car_name=:car_name,car_type=:car_type,capacity=:capacity,price=:price,gas_type=:gas_type WHERE car_id=$1`
-	_, err := db.NamedExec(query, cardata)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
 	router := gin.Default()
 	router.GET("/Cars", getallCars)
@@ -140,15 +80,4 @@ func main() {
 	router.DELETE("/Cars/:id", deleteCar)
 	router.PUT("/Cars/:id", updateCar)
 	router.Run("localhost:8080")
-	// data := db.Data()
-	// fmt.Println(data)
-}
-
-func DBConnect() *sqlx.DB {
-	db, err := sqlx.Connect("postgres", "user=postgres password=cactus1470 host=localhost port=5432 dbname=carrental sslmode=disable")
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return db
 }
