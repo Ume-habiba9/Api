d19c1ecea981acc9b541bc59518d282ff2021438
Ume-habiba9 <habibaume571@gmail.com>
2023-01-20 16:02:52 +0500

    id removed

--- api/auth.go
+++ api/auth.go
@@ -2,27 +2,28 @@ package api
 
 import (
 	"net/http"
+
 	"github.com/Ume-habiba9/Api/db"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	
 )
+
 func LogIn(c *gin.Context) {
-email := c.Param("email")
-passward := c.Param("passward")
-var logInDetails db.User
-if err := c.ShouldBindJSON(&logInDetails); err != nil {
-	c.JSON(http.StatusInternalServerError, "Invalid User Data")
-	return
-}
-_, err := db.LogInCheck(email, passward)
-if err != nil {
-	c.JSON(http.StatusInternalServerError, err)
-	return
-}
-if (logInDetails.Email != email) || (logInDetails.Password != passward) {
-	c.JSON(http.StatusOK, "Invalid Data")
-} else {
-	c.JSON(http.StatusOK, "User Exists")
-}
+	email := c.Param("email")
+	passward := c.Param("passward")
+	var logInDetails db.User
+	if err := c.ShouldBindJSON(&logInDetails); err != nil {
+		c.JSON(http.StatusInternalServerError, "Invalid User Data")
+		return
+	}
+	_, err := db.LogInCheck(email, passward)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	if (logInDetails.Email != email) || (logInDetails.Password != passward) {
+		c.JSON(http.StatusOK, "Invalid Data")
+	} else {
+		c.JSON(http.StatusOK, "User Exists")
+	}
 }
--- db/auth.go
+++ db/auth.go
@@ -5,7 +5,7 @@ func LogInCheck(email, passward string) ([]*User, error) {
 	defer database.Close()
 	userData := make([]*User, 0)
 	query := `SELECT user_id,email,passward FROM carrental.users WHERE email=$1 AND passward=$2`
-	err := database.Select(&userData, query ,email, passward)
+	err := database.Select(&userData, query, email, passward)
 	if err != nil {
 		return nil, err
 	}
