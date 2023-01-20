f3a761290b4611465b6ed075c5a3ea073399a779
Ume-habiba9 <habibaume571@gmail.com>
2023-01-20 15:48:02 +0500

    changes added

--- api/auth.go
+++ api/auth.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-
 	"github.com/Ume-habiba9/Api/db"
-
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	
@@ -14,8 +11,8 @@ func LogIn(c *gin.Context) {
 id := c.Param("id")
 email := c.Param("email")
 passward := c.Param("passward")
-var logindetails db.User
-if err := c.ShouldBindJSON(&logindetails); err != nil {
+var logInDetails db.User
+if err := c.ShouldBindJSON(&logInDetails); err != nil {
 	c.JSON(http.StatusInternalServerError, "Invalid User Data")
 	return
 }
@@ -24,8 +21,7 @@ if err != nil {
 	c.JSON(http.StatusInternalServerError, err)
 	return
 }
-fmt.Println(logindetails)
-if (logindetails.Email != email) || (logindetails.Password != passward) {
+if (logInDetails.Email != email) || (logInDetails.Password != passward) {
 	c.JSON(http.StatusOK, "Invalid Data")
 } else {
 	c.JSON(http.StatusOK, "User Exists")
--- db/auth.go
+++ db/auth.go
@@ -1,20 +1,15 @@
 package db
 
-import (
-	"fmt"
-)
-
 func LogInCheck(id, email, passward string) ([]*User, error) {
 	database := DBConnect()
 	defer database.Close()
-	user := make([]*User, 0)
+	userData := make([]*User, 0)
 	query := `SELECT user_id,email,passward FROM carrental.users WHERE user_id=$1 AND email=$2 AND passward=$3`
-	err := database.Select(&user, query, id, email, passward)
-	fmt.Println(err)
+	err := database.Select(&userData, query, id, email, passward)
 	if err != nil {
 		return nil, err
 	}
-	return user, err
+	return userData, err
 }
 
 
